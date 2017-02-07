193ce366379efb0ad38cf87a352c6a580a45d9b9
Nate <nate4495@gmail.com>
2017-02-06 21:32:11 -0500

    artwork updated

--- line-of-God/js/main.js
+++ line-of-God/js/main.js
@@ -13,7 +13,7 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1000, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
 
